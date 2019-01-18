5c5e4c44bf5ad1c6ee21a5e19e1f9e312bf39370
Marcus Schuster <schustermarcus1975@gmail.com>
2019-01-18 07:14:22 -0500

    Adds register, adds login and auth middleware routes on server

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Rental = require("../models/rental");
+const UserCtrl = require("../controllers/user");
+
+router.get("/secret", UserCtrl.authMiddleware, function(req, res) {
+  res.json({ secret: true });
+});
 
 router.get("", function(req, res) {
   Rental.find({}, function(err, foundRentals) {
@@ -12,11 +17,9 @@ router.get("/:id", function(req, res) {
   const rentalId = req.params.id;
   Rental.findById(rentalId, function(err, foundRental) {
     if (err) {
-      res
-        .status(422)
-        .send({
-          errors: [{ title: "Rental Error!", detail: "Could not find Rental!" }]
-        });
+      res.status(422).send({
+        errors: [{ title: "Rental Error!", detail: "Could not find Rental!" }]
+      });
     }
     res.json(foundRental);
   });
